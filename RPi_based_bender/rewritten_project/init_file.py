fffe6328f3580caaf087c7bc066e1ad357ae4de7
qdree <ta4ckpih@gmail.com>
2017-01-20 16:49:28 +0200

    added simple parser fro json

--- RPi_based_bender/rewritten_project/init_file.py
+++ RPi_based_bender/rewritten_project/init_file.py
@@ -1,5 +1,4 @@
-import RPi.GPIO as GPIO
-import json
+#import RPi.GPIO as GPIO
 import time
 
 FEED_MOTOR_STEPS_PER_REVOLUTION = 4000 
--- RPi_based_bender/rewritten_project/main_program.py
+++ RPi_based_bender/rewritten_project/main_program.py
@@ -1,13 +1,19 @@
 from init_file import *
+import json
 
-setup()
+# setup()
 #wait(5)
-homingRoutine()
-for i in range(2):
-	feedWire(50)
-	wait(0.001)
-	bendWire(45)
-	wait(0.001)	
+# homingRoutine()
+with open('json_sample.json') as data_file:    
+    data = json.load(data_file)
+
+print data
+
+# for i in range(2):
+# 	feedWire(50)
+# 	wait(0.001)
+# 	bendWire(45)
+# 	wait(0.001)	
 	
 
 wait(5)
